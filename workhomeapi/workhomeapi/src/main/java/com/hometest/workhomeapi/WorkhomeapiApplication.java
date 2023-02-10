51479cf397d21bfdfad4e0a5fc2f2fcc257dbf7d
Nguyễn Huy Quang <35794747+Quangnguyen97@users.noreply.github.com>
2023-02-10 16:17:00 +0700

    Add files via upload

---
+++ workhomeapi/workhomeapi/src/main/java/com/hometest/workhomeapi/WorkhomeapiApplication.java
@@ -0,0 +1,13 @@
+package com.hometest.workhomeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkhomeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkhomeapiApplication.class, args);
+	}
+
+}
---
+++ workhomeapi/workhomeapi/src/test/java/com/hometest/workhomeapi/WorkhomeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hometest.workhomeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkhomeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
