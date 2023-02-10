0b452a6db426eef586e353570d43041867a9f1f6
15110107_ Nguyễn Huy Quang <15110107@student.hcmute.edu.vn>
2023-02-10 23:34:01 +0700

    .

---
+++ hometest/hometest/hometest/src/main/java/com/example/hometest/AccountServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.hometest;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.hometest.Account.Account;
+import com.example.hometest.Account.AccountRepository;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    private AccountRepository accountRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        super();
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public Account saveAccount(Account Account) {
+        return accountRepository.save(Account);
+    }
+
+    @Override
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    @Override
+    public Account getAccountByUserId(long UserId) {
+        return accountRepository.findById(UserId).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "UserId", UserId));
+    }
+
+    @Override
+    public Account updateAccount(Account Account, long UserId) {
+
+        Account existingAccount = accountRepository.findById(UserId).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "UserId", UserId));
+
+        existingAccount.setAccountNumber(Account.getAccountNumber());
+        existingAccount.setBalance(Account.getBalance());
+        accountRepository.save(existingAccount);
+        return existingAccount;
+    }
+
+    @Override
+    public void deleteAccount(long UserId) {
+        accountRepository.findById(UserId).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "UserId", UserId));
+        accountRepository.deleteById(UserId);
+    }
+}
---
+++ hometest/hometest/hometest/src/main/java/com/example/hometest/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package com.example.hometest;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.hometest.User.User;
+import com.example.hometest.User.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        super();
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User saveUser(User User) {
+        return userRepository.save(User);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserByUserId(long Userid) {
+        return userRepository.findById(Userid).orElseThrow(
+                () -> new ResourceNotFoundException("User", "Userid", Userid));
+    }
+
+    @Override
+    public User updateUser(User User, long Userid) {
+
+        User existingUser = userRepository.findById(Userid).orElseThrow(
+                () -> new ResourceNotFoundException("User", "Userid", Userid));
+
+        existingUser.setFullName(User.getFullName());
+        existingUser.setPassword(User.getPassword());
+        existingUser.setNotificationToken(User.getNotificationToken());
+        userRepository.save(existingUser);
+        return existingUser;
+    }
+
+    @Override
+    public void deleteUser(long Userid) {
+        userRepository.findById(Userid).orElseThrow(
+                () -> new ResourceNotFoundException("User", "Userid", Userid));
+        userRepository.deleteById(Userid);
+    }
+}
