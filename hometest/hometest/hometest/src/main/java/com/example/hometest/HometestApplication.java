d1f14df060b33d524b6ffd2774d04585e58a6a95
15110107_ Nguyễn Huy Quang <15110107@student.hcmute.edu.vn>
2023-02-10 23:12:35 +0700

    .

--- hometest/hometest/hometest/src/main/java/com/example/hometest/HometestApplication.java
+++ hometest/hometest/hometest/src/main/java/com/example/hometest/HometestApplication.java
@@ -2,8 +2,12 @@ package com.example.hometest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.example.hometest.Account" })
+@ComponentScan(basePackages = { "com.example.hometest.User" })
+
 public class HometestApplication {
 
 	public static void main(String[] args) {
