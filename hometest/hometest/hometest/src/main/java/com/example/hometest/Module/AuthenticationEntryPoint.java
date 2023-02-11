b42f93ec1b4eb14796953a177a33f8fcbd856432
15110107_ Nguyễn Huy Quang <15110107@student.hcmute.edu.vn>
2023-02-12 01:46:19 +0700

    .

--- hometest/hometest/hometest/src/main/java/com/example/hometest/Module/AuthenticationEntryPoint.java
+++ hometest/hometest/hometest/src/main/java/com/example/hometest/Module/AuthenticationEntryPoint.java
@@ -11,6 +11,21 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-public class AuthenticationEntryPoint {
+@Component
+public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+            throws IOException, ServletException {
+        response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status 401 - " + authEx.getMessage());
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        setRealmName("DeveloperStack");
+        super.afterPropertiesSet();
+    }
 }
--- hometest/hometest/hometest/src/main/java/com/example/hometest/Module/SpringSecurityConfig.java
+++ hometest/hometest/hometest/src/main/java/com/example/hometest/Module/SpringSecurityConfig.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +24,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Autowired
         public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-                auth.inMemoryAuthentication().withUser("john123").password("password").roles("USER");
+                auth.inMemoryAuthentication()
+                                .withUser("admin")
+                                .password("123456")
+                                .roles("USER");
         }
 }
