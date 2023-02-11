d69f2c2ccd03b92a197ad6f57ed9a7a509c7510a
15110107_ Nguyễn Huy Quang <15110107@student.hcmute.edu.vn>
2023-02-12 01:22:00 +0700

    .

--- hometest/hometest/hometest/src/main/java/com/example/hometest/Module/SpringSecurityConfig.java
+++ hometest/hometest/hometest/src/main/java/com/example/hometest/Module/SpringSecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class SpringSecurityConfig {
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
