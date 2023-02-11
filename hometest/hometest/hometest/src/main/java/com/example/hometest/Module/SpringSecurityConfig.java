d3a6c29ca1465af1b2ba39dc3e987b02f9575023
15110107_ Nguyễn Huy Quang <15110107@student.hcmute.edu.vn>
2023-02-12 01:36:17 +0700

    .

--- hometest/hometest/hometest/src/main/java/com/example/hometest/Module/SpringSecurityConfig.java
+++ hometest/hometest/hometest/src/main/java/com/example/hometest/Module/SpringSecurityConfig.java
@@ -1,36 +1,31 @@
 package com.example.hometest.Module;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                http.authorizeHttpRequests((requests) -> requests
-                                .requestMatchers("/").permitAll()
-                                .anyRequest().authenticated())
-                                .formLogin((form) -> form.permitAll())
-                                .logout((logout) -> logout.permitAll());
-                return http.build();
+        @Autowired
+        private AuthenticationEntryPoint authEntryPoint;
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+                http.csrf().disable().authorizeRequests()
+                                .anyRequest().authenticated()
+                                .and().httpBasic()
+                                .authenticationEntryPoint(authEntryPoint);
         }
 
-        @Bean
-        public UserDetailsService userDetailsService() {
-                UserDetails user = User.withDefaultPasswordEncoder()
-                                .username("admin")
-                                .password("123456")
-                                .roles("USER")
-                                .build();
-                return new InMemoryUserDetailsManager(user);
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+                auth.inMemoryAuthentication().withUser("john123").password("password").roles("USER");
         }
 }
