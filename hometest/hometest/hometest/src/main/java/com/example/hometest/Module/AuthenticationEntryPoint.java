a5d825642498122b53076754df7096d48ca8c7fa
15110107_ Nguyễn Huy Quang <15110107@student.hcmute.edu.vn>
2023-02-12 12:49:42 +0700

    .

--- hometest/hometest/hometest/src/main/java/com/example/hometest/Module/AuthenticationEntryPoint.java
+++ hometest/hometest/hometest/src/main/java/com/example/hometest/Module/AuthenticationEntryPoint.java
@@ -1,23 +1,25 @@
 package com.example.hometest.Module;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.apache.catalina.security.SecurityConfig;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Component
 public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
-            throws IOException, ServletException {
-        response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+    public void commence(
+      HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+      throws IOException, ServletException {
+        response.addHeader("WWW-Authenticate", "Basic realm="" + getRealmName() + """);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         PrintWriter writer = response.getWriter();
         writer.println("HTTP Status 401 - " + authEx.getMessage());
@@ -25,7 +27,7 @@ public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        setRealmName("DeveloperStack");
+        setRealmName("administrator");
         super.afterPropertiesSet();
     }
 }
